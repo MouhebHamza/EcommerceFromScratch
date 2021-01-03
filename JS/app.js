c38772785fa01a5deed1aa34b8720c19f27e6d26
mouheb <mbmouheb@gmail.com>
2021-01-03 16:28:52 +0100

    first commit

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+$("#loginBtn").click(function(){
+    $('#loginModal').modal('toggle')
+});
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
